478a7cd709ca06a9fff35ff6264a618e7ab9b0d4
Mohamed Diab <helmy.diab@gmail.com>
2022-03-24 08:53:16 +0400

    add editorconfig and its plugin

---
+++ index.js
@@ -0,0 +1,12 @@
+function testing() {}
+function test2() {}
+
+const x = () => {
+  let t, y;
+  if (t === 1) {
+    console.log("1");
+    console.log("2");
+  } else {
+    console.log("3");
+  }
+};
